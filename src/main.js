45bc51264df5b54904affccbcdc50f9d5a98328c
Ashley Siu <ashley.c.siu@gmail.com>
2020-10-30 21:47:48 -0700

    creates desserts class with type and calories properties

---
+++ src/main.js
@@ -0,0 +1,8 @@
+//Create a class called Desserts and pass it two properties called "type" and "calories"
+
+class Desserts {
+  constructor (type, calories) {
+    this.type = type;
+    this.calories = calories;
+  }
+}
